2589a10f50ef40475255675dea65c7a708e9074a
josprox <pinguin69eyes@gmail.com>
2022-06-12 18:22:27 -0500

    Proyecto js 1.0

---
+++ js/mostrar.js
@@ -0,0 +1,9 @@
+function registro() {
+    document.getElementById('registro').style.display = 'block';
+    document.getElementById('login').style.display = 'none';
+}
+
+function login() {
+    document.getElementById('registro').style.display = 'none';
+    document.getElementById('login').style.display = 'block';
+}
