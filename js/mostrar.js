e8f003fd3d43351404a6dd9ac69169dd13496618
josprox <pinguin69eyes@gmail.com>
2022-06-12 19:04:50 -0500

    Proyecto js full

--- js/mostrar.js
+++ js/mostrar.js
@@ -1,9 +1,13 @@
 function registro() {
     document.getElementById('registro').style.display = 'block';
     document.getElementById('login').style.display = 'none';
+    $('.active').removeClass("active");
+    $("#mst").addClass("active");
 }
 
 function login() {
     document.getElementById('registro').style.display = 'none';
     document.getElementById('login').style.display = 'block';
+    $('.active').removeClass("active");
+    $("#alm").addClass("active");
 }
